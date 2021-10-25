09760d277fbfa4bfa798aa1b062a1a1fa957a6f8
Andres Roldan <dresroldan@gmail.com>
2021-10-25 18:36:45 -0500

    Setup error and success function

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const lname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+// form fields and submit event listener
+
 form.addEventListener('submit', function (e) {
+  // prevent the form from submitting
   e.preventDefault();
 
   let firstName = fname.value.trim();
@@ -12,5 +15,28 @@ form.addEventListener('submit', function (e) {
   let email = fname.value.trim();
   let password = fname.value.trim();
 
-  console.log(firstName);
+  if (firstName === '') {
+    showError(fname, 'First Name cannot be empty');
+  } else {
+    showSuccess(fname);
+  }
 });
+
+// showError() highlights the border of the input field and displays an error message if the input field is invalid:
+
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  const span = formField.querySelector('span');
+
+  span.innerText = message;
+
+  span.classList.add('error-message');
+  input.classList.add('error');
+};
+
+// showSuccess() removes the error class, adds the success class, and set the error message to blank.
+
+const showSuccess = (input) => {
+  input.classList.remove('error');
+  input.classList.add('success');
+};
