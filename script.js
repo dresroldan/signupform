c2ed673aee64c4ccc1068f0b131e0254b7e018c6
Andres Roldan <dresroldan@gmail.com>
2021-10-27 17:00:06 -0500

    Consolidated functions for simplicity

--- script.js
+++ script.js
@@ -4,63 +4,45 @@ const lastNameEl = document.getElementById('lastname');
 const emailEl = document.getElementById('email');
 const passwordEl = document.getElementById('password');
 
-const checkFirstName = () => {
+// form submit event listener
+
+form.addEventListener('submit', function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+
   let firstName = firstNameEl.value.trim();
-
-  let valid = false;
-
-  if (!isRequired(firstName)) {
-    showError(firstNameEl, 'First name cannot be blank.');
-  } else {
-    showSuccess(firstNameEl);
-    valid = true;
-  }
-  return valid;
-};
-
-const checkLastName = () => {
   let lastName = lastNameEl.value.trim();
-
-  let valid = false;
-
-  if (!isRequired(lastName)) {
-    showError(lastNameEl, 'First name cannot be blank.');
-  } else {
-    showSuccess(lastNameEl);
-    valid = true;
-  }
-  return valid;
-};
-
-const checkEmail = () => {
   let email = emailEl.value.trim();
-
-  let valid = false;
-
-  if (!isRequired(email)) {
-    showError(emailEl, 'First name cannot be blank.');
-  } else {
-    showSuccess(emailEl);
-    valid = true;
-  }
-  return valid;
-};
-
-const checkPassword = () => {
   let password = passwordEl.value.trim();
 
-  let valid = false;
+  if (firstName === '') {
+    showError(firstNameEl, 'First Name is required');
+  } else {
+    showSuccess(firstNameEl);
+  }
 
-  if (!isRequired(password)) {
-    showError(passwordEl, 'First name cannot be blank.');
+  if (lastName === '') {
+    showError(lastNameEl, 'Last Name is required');
+  } else {
+    showSuccess(lastNameEl);
+  }
+
+  if (email === '') {
+    showError(emailEl, 'Email Address cannot be blank.');
+  } else if (!isEmailValid(email)) {
+    showError(emailEl, 'Email is not valid');
+  } else {
+    showSuccess(emailEl);
+  }
+
+  if (password === '') {
+    showError(passwordEl, 'Password cannot be blank.');
+  } else if (!isPasswordSecure(password)) {
+    showError(passwordEl, 'Password is not valid');
   } else {
     showSuccess(passwordEl);
-    valid = true;
   }
-  return valid;
-};
-
-const isRequired = (value) => (value === '' ? false : true);
+});
 
 // showError() highlights the border of the input field and displays an error message if the input field is invalid:
 const showError = (input, message) => {
@@ -87,14 +69,15 @@ const showSuccess = (input) => {
   error.textContent = '';
 };
 
-// form submit event listener
+const isEmailValid = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
 
-form.addEventListener('submit', function (e) {
-  // prevent the form from submitting
-  e.preventDefault();
-
-  let isFirstNameValid = checkFirstName();
-  let isLastNameValid = checkLastName();
-  let isPasswordValid = checkPassword();
-  let isEmailValid = checkEmail();
-});
+const isPasswordSecure = (password) => {
+  const re = new RegExp(
+    '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'
+  );
+  return re.test(password);
+};
